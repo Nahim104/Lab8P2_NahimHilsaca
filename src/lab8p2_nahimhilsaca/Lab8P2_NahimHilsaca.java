0f94185ded0829eb37244be5adfba4e9d4e7eb6a
Nahim104 <nahimhilsaca1945@gmail.com>
2023-09-08 13:48:49 -0600

    Creacion del proyecto

---
+++ src/lab8p2_nahimhilsaca/Lab8P2_NahimHilsaca.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package lab8p2_nahimhilsaca;
+
+/**
+ *
+ * @author nahim
+ */
+public class Lab8P2_NahimHilsaca {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
